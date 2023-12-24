3e1e406b3fcecb2d09926ab2b65cfeb7fd0cd044
Jitendra <info.jiitendra@gmail.com>
2023-12-24 16:00:17 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
 
-export default function App() {
-  return <h1>Running</h1>;
-}
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="519b7b73-7771-431b-97c7-4ff61bae4e72"
+      userName="Jitendra"
+      userSecret="123J"
+    />
+  );
+};
+
+export default App;
